fb52f2b1208731dd85c7c398a20551f70eae513e
dgm000624 <144201185+dgm000624@users.noreply.github.com>
2025-10-29 10:30:58 +0900

    Update Cds.c

--- AVR/Project_meditator/Project_meditator/Cds/Cds.c
+++ AVR/Project_meditator/Project_meditator/Cds/Cds.c
@@ -9,15 +9,20 @@
 #include "../UART/UART.h"
 #include "../Timer/Timer.h"
 
-
+# 광센서 수치 기록용
 unsigned int adc_data = 0;
+
+# 상태 기록용(낮, 밤)
 int day = 1;
 
+#광센서 감지 인터럽트
 ISR(ADC_vect)
-{
+{	
+	#광센서에 기록된 수치
 	adc_data = ADCW;
 	ADCSRA = ADCSRA | 0x40;
-	
+
+	# 광센서가 200이하가 되었고 이전이 낮이었으면 실행 / 광센서가 700초과가 되었고 이전이 밤이었으면 실행
 	if(adc_data <200 && day == 1)
 	{
 		USART0_str("\r\nNight comes\r\n");
